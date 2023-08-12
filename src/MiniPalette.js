00b9d8579b4e0b55048a8369de0a62106b4068cb
Sonata7245 <Sonata7245@gmail.com>
2023-08-12 12:38:18 -0700

    finish styling mini palettes

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -5,6 +5,7 @@ import withStyles from '@material-ui/styles/withStyles';
 const styles = {
     root: {
         backgroundColor: "white",
+        border: "1px solid black",
         borderRadius: "5px",
         padding: "0.5rem",
         position: "relative",
@@ -14,7 +15,11 @@ const styles = {
         }
     },
     colors:{
-        backgroundColor: "grey"
+        backgroundColor: "#dae1e4",
+        height: "150px",
+        width: "100%",
+        borderRadius: "5px",
+        overflow: "hidden"
     },
     title:{
         display: "flex",
@@ -28,14 +33,24 @@ const styles = {
     emoji:{
         marginLeft: "0.5rem",
         fontSize: "1.5rem"
+    },
+    miniColor:{
+        height: "25%",
+        width: "20%",
+        display: "inline-block",
+        margin: "0 auto",
+        position: "relative",
+        marginBottom: "-4px"
     }
 }
 
 function MiniPalette(props){
-    const {classes, paletteName, emoji} = props;
+    const {classes, paletteName, emoji, colors} = props;
     return(
         <div className={classes.root}>
-            <div className={classes.colors}></div>
+            <div className={classes.colors}>
+                {colors.map(c => <div className={classes.miniColor} key={c.name} style={{backgroundColor: c.color}}></div>)}
+            </div>
             <h5 className={classes.title}>{paletteName}<span className={classes.emoji}>{emoji}</span></h5>
         </div>
     )
