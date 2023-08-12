a45c167cc7a082d51bee1552aa7e07926308a060
Sonata7245 <Sonata7245@gmail.com>
2023-08-12 09:08:20 -0700

    add snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react'
 import Select from "@material-ui/core/Select"
 import MenuItem from "@material-ui/core/MenuItem"
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close'
+import IconButton from '@material-ui/core/IconButton'
 
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
+import { Icon } from '@material-ui/core';
+
 
 
 class Navbar extends Component{
     constructor(props){
         super(props);
-        this.state = {format: "hex"}
+        this.state = {format: "hex", open: false}
         this.handleChange = this.handleChange.bind(this)
+        this.closeSnackbar = this.closeSnackbar.bind(this)
     }
     handleChange(e){
-        this.setState({ format: e.target.value });
+        this.setState({ format: e.target.value, open: true });
         this.props.handleChange(e.target.value);
     }
+    closeSnackbar(){
+        this.setState({open: false});
+    }
+
     render(){
         const {level, changeLevel} = this.props
         const {format} = this.state
@@ -44,6 +54,16 @@ class Navbar extends Component{
                         <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
                     </Select>
                 </div>
+                <Snackbar 
+                    anchorOrigin={{vertical: "bottom", horizontal: "left"}} 
+                    open={this.state.open}
+                    onClose={this.closeSnackbar}
+                    autoHideDuration={3000}
+                    message={<span id="message">Format Changed</span>}
+                    ContentProps={{"aria-describedby": "message-id"}}
+                    action={[<IconButton onClick={this.closeSnackbar} color='inherit' aria-label="close"><CloseIcon /></IconButton>]}
+                    />
+                    
             </header>
         )
     }
