e8be97a1f0b955d96470c19b57599b847c83a350
Sonata7245 <Sonata7245@gmail.com>
2023-08-12 12:26:42 -0700

    styling home

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import MiniPalette from './MiniPalette'
+import withStyles from '@material-ui/styles/withStyles';
+
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    container:{
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
+    },
+    nav:{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        color: "white"
+    },
+    palettes:{
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%"
+    }
+}
 
 class Home extends Component{
     render(){
-        const {palettes} = this.props
+        const {palettes, classes} = this.props
         return(
-            <div>
-            <h1>React Colors</h1>
-            {palettes.map(palette => (
-                <MiniPalette {...palette}/>
-                
-            ))}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>React Colors</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map(palette => (
+                        <MiniPalette {...palette}/>
+                        ))}
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default Home
+export default withStyles(styles)(Home);
