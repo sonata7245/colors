6da2c27017fc60d2fd8c28d53bc1a027d8ce0303
Sonata7245 <Sonata7245@gmail.com>
2023-08-14 21:57:06 -0700

    add styles and delete to palette form

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     return (
 
       <Switch>
-        <Route exact path="/palette/new" render={(routeProps) => <NewPaletteForm savePalette={this.savePalette} {...routeProps} />}/>
+        <Route exact path="/palette/new" render={(routeProps) => <NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />}/>
         <Route exact path='/' render={(routeProps)=> <Home palettes={this.state.palettes} {...routeProps}/>}></Route>
         <Route exact path='/palette/:id' render={(routeProps)=> <Palette  palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
         <Route exact path="/palette/:paletteId/:colorId" render={(routeProps)=> <SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))} />}/>
--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import withStyles from '@material-ui/styles/withStyles'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const styles = {
     root: {
@@ -10,14 +11,38 @@ const styles = {
         position: "relative",
         cursor: "pointer",
         marginBottom: "-4px",
+        "&:hover svg": {
+            color: "white",
+            transform: "scale(1.5)"
+        }
     },
+    boxContent: {
+        position: "absolute",
+        width: "100%",
+        left: "0px",
+        bottom: "0px",
+        padding: "10px",
+        color: "rgba(0,0,0,0.5)",
+        textTransform: "uppercase",
+        letterSpacing: "1px",
+        fontSize: "12px",
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    deleteIcon: {
+        transition: "all 0.3s ease-in-out"
+    }
 }
 
 function DraggableColorBox(props){
+    const {classes, handleClick, name, color} = props
     return (
-        <div className={props.classes.root} style={{backgroundColor: props.color}}>
-            {props.color}
-            {props.name}
+        <div className={classes.root} style={{backgroundColor: color}}>
+           <div className={classes.boxContent}>
+            <span>{name}</span>
+            <DeleteIcon className={classes.deleteIcon} onClick={handleClick}/>
+            </div>
+            
         </div>
     )
 }
